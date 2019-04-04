2ffa793ee0316bbbb634d3381890f64329486253
Merve <47145223+mervegulozcan@users.noreply.github.com>
2019-04-04 17:28:20 +0300

    First Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace BeniYakala
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnHedef = new System.Windows.Forms.Button();
+            this.ımageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // btnHedef
+            // 
+            this.btnHedef.Location = new System.Drawing.Point(308, 170);
+            this.btnHedef.Name = "btnHedef";
+            this.btnHedef.Size = new System.Drawing.Size(85, 67);
+            this.btnHedef.TabIndex = 0;
+            this.btnHedef.UseVisualStyleBackColor = true;
+            this.btnHedef.Click += new System.EventHandler(this.btnHedef_Click);
+            this.btnHedef.MouseEnter += new System.EventHandler(this.btnHedef_MouseEnter);
+            // 
+            // ımageList1
+            // 
+            this.ımageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList1.ImageStream")));
+            this.ımageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.ımageList1.Images.SetKeyName(0, "images.jpg");
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 462);
+            this.Controls.Add(this.btnHedef);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "0";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Click += new System.EventHandler(this.Form1_Click);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnHedef;
+        private System.Windows.Forms.ImageList ımageList1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BeniYakala
+{
+    public partial class Form1 : Form
+    {
+        int hamle;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnHedef.Image = ımageList1.Images[0];
+
+        }
+
+        private void btnHedef_Click(object sender, EventArgs e)
+        {
+            
+            MessageBox.Show(string.Format($"Tebrikler {hamle}. hamlede tıkladınız"));
+            hamle = 0;
+            Text = hamle.ToString();
+        }
+
+        private void btnHedef_MouseEnter(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+
+            btnHedef.Left = rnd.Next(0, this.ClientSize.Width - btnHedef.ClientSize.Width);
+            btnHedef.Top = rnd.Next(0, this.ClientSize.Height - btnHedef.ClientSize.Height);
+
+
+        }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            hamle++;
+            Text = hamle.ToString();
+        }
+    }
+}
